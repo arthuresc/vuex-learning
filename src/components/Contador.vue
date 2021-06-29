4ee7a0131b743e379145d9ada568b5150ce835a4
Arthur Escalera <arthur@tideal.com.br>
2021-06-29 13:19:03 -0300

    [1 VUEX] Primeiro commit

---
+++ src/components/Contador.vue
@@ -0,0 +1,41 @@
+<template>
+  <div>
+    <h2 class="font-weight-light">Contador: {{ contador }}</h2>
+    <button class="btn bn--lg btn-danger mr-1" @click="decrementar">-</button>
+    <button class="btn bn--lg btn-success" @click="incrementar">+</button>
+    <h1>{{ contadorMultiplicado }}</h1>
+  </div>
+</template>
+
+<script>
+import { mapState } from 'vuex';
+
+export default {
+  // computed: mapState([ 'contador' ]),
+  // computed: {
+  // ...mapState([ 'contador' ]),
+  // },
+  data() {
+    return {
+    };
+    
+  },
+  computed: {
+    ...mapState({
+      contador: 'contador',
+      alias: 'contador',
+      contadorMultiplicado(state) {
+        return state.contador * this.contadorLocal; //não pode arrow function
+      },
+    }),
+  },
+  methods: {
+    decrementar() {
+      this.$store.state.contador--;
+    },
+    incrementar() {
+      this.$store.state.contador++;
+    },
+  },
+};
+</script>
